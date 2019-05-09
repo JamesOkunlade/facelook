425b7295e836285d891c517549ada6db13ea5b5d
Adnan Afsari <aarahul1991@gmail.com>
2019-05-09 17:34:10 +0600

    first and last name validation added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, length: { maximum: 20 },
+  format: { with: /[a-z]+/i }
+  validates :last_name, presence: true, length: { maximum: 20 },
+  format: { with: /[a-z]+/i }
 end
