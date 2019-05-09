4f63b5b68997f9b04b0ce40e076383abbaf04247
Adnan Afsari <aarahul1991@gmail.com>
2019-05-10 01:29:13 +0600

    friends request associations added

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,3 +1,5 @@
 class FriendRequest < ApplicationRecord
-  
+
+  belongs_to :sender, class_name: "User"
+  belongs_to :receiver, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  has_many :friend_requests_sent, foreign_key: :sender_id,
+          class_name: "FriendRequest", dependent: :destroy
+
+  has_many :friend_requests_received, foreign_key: :receiver_id,
+          class_name: "FriendRequest", dependent: :destroy
+
 
 end
