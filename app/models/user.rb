7d0073f6c4a54b47862a972794dcdfc85b8e432c
James Okunlade <james.okunlade@gmail.com>
2019-05-09 13:00:23 +0100

    User association with Post added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   format: { with: /[a-z]+/i }
   validates :last_name, presence: true, length: { maximum: 20 },
   format: { with: /[a-z]+/i }
+
+  has_many :posts, dependent: :destroy
+
 end
