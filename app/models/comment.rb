fa178b5221440edc8671b909492e6b5d6fae4546
Adnan Afsari <aarahul1991@gmail.com>
2019-05-09 20:34:14 +0600

    validations for comment added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ApplicationRecord
-
+  validates :content, presence: true, length: { maximum: 150 }
+  validates :post, presence: true
+  validates :user, presence: true
+  
   belongs_to :post
   belongs_to :user
 end
