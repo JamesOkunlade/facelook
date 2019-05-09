e50060df072c61e20cc5197bf3a987df95a900e6
Adnan Afsari <aarahul1991@gmail.com>
2019-05-10 01:31:02 +0600

    friend request validation added

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,5 +1,7 @@
 class FriendRequest < ApplicationRecord
-
+  validates :sender, presence: true
+  validates :receiver, presence: true
+  
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
 end
