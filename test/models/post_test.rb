486bdecc8eb09eb3b3a0b7b353b22b9342845f6f
James Okunlade <james.okunlade@gmail.com>
2019-05-14 09:11:38 +0100

    Post unit test completed

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,6 +4,18 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  
+  def setup
+    @user = users(:james)
+    @post = Post.new(content: "This is a test content", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
 
 end
