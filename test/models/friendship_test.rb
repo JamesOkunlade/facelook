ce69d03a82b440bc1ea73352fed3aaacd123981e
Adnan Afsari <aarahul1991@gmail.com>
2019-05-15 18:03:34 +0600

    test for friendship model is added

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @adder = users(:james)
+    @friendship = Friendship.new(adder_id: @adder.id)
+  end
+
+  test "receiver should be present" do
+    assert_not @friendship.valid?
+  end
 end
